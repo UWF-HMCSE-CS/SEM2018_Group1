8e4dcad35c3113fdd95c858fd8818b163bbd3092
Nova Reaper <chrystechsystems@gmail.com>
2018-10-02 15:02:39 -0500

    nodejs copies1

--- routes.js
+++ routes.js
@@ -2,4 +2,8 @@ var passport = require('passport');
 var router = require('express').Router();
 var request = require("request");
 
+router.get('/login', function(req,res){
+	res.render('login',{});
+})
+
 module.exports = router
