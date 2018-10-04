d17257f2d487c4d79c44265d6917a7089a118c2b
Nova Reaper <chrystechsystems@gmail.com>
2018-10-04 14:56:50 -0500

    lots of love

--- routes.js
+++ routes.js
@@ -7,10 +7,43 @@ router.get('/login', function(req,res){
 });
 router.post('/login', function(req,res){
 	console.log(req.body);
-	if(req.body.login)
+	if(req.body.login){
+		console.log("LOGIN ROUTE");
 		res.redirect('/');
-	else if (req.body.register)
+	}
+	else if (req.body.register){
+		console.log("REGISTER ROUTE");
 		res.redirect('/register');
+	}
+	else res.redirect('/');
 });
+router.get('/league',function(req,res){
+	res.render('league',{});
+})
+router.get('/players',function(req,res){
+	res.render('players',{});
+})
+router.get('/team',function(req,res){
+	res.render('team',{});
+})
+
+//until fixed
+router.get('/index.html',function(req,res){
+	res.redirect('/');
+})
+router.get('/players.html',function(req,res){
+	res.redirect('/players');
+})
+router.get('/team.html',function(req,res){
+	res.redirect('/team');
+})
+router.get('/login.html',function(req,res){
+	res.redirect('/login');
+})
+
+router.get('/',function(req,res){
+	res.render('home',{});
+})
+
 
 module.exports = router
