aad4f4bddd0bfd5ec1ca94d6ee88eda6f3497c4c
Nova Reaper <chrystechsystems@gmail.com>
2018-10-19 13:52:48 -0500

    add database test

--- tests/tests.js
+++ tests/tests.js
@@ -1,4 +1,4 @@
-let dbcon = require('/lib/mysqlDBMgr.js');
+let dbcon = require('../lib/mysqlDBMgr.js');
 let assert = require('assert');
 
 dbcon.query('select * from user;', function(err,rows,cols){
