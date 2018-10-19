0f70599e0099b4ea6ae183e5e87d910d2abc3b0f
Nova Reaper <chrystechsystems@gmail.com>
2018-10-19 14:03:43 -0500

    attempt to debug test issue

--- tests/tests.js
+++ tests/tests.js
@@ -2,6 +2,6 @@ let dbcon = require('../lib/mysqlDBMgr.js');
 let assert = require('assert');
 
 dbcon.query('select * from user;', function(err,rows,cols){
-    console.log(err);
+    console.log("error is: " + err);
     assert(err,null);
 });
