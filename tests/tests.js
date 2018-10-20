ee2b31079d7b8f1df02b9d35d559f176887d7b56
Nova Reaper <chrystechsystems@gmail.com>
2018-10-19 20:12:59 -0500

    A lot of tests for login

--- tests/tests.js
+++ tests/tests.js
@@ -1,8 +1,80 @@
 let dbcon = require('../lib/mysqlDBMgr.js');
 let assert = require('assert');
 
-dbcon.query('select * from user;', function(err,rows,cols){
-    console.log("error is: " + err);
-    if(err) throw err;
-});
+var numberOfErrors = 0;
+try {
+    dbcon.query('select * from user;', function (err, rows, cols) {
+        console.log("error is: " + err);
+        if (err) throw err;
+    });
+}catch(e){
+    console.error(e);
+    numberOfErrors ++;
+}
+try{
+    let username = "TestUsername";
+    let email = "testEmail@testdomain.com";
+    let password = "testPassword";
+    dbcon.query('insert into login values(?,AES_ENCRYPT(?,?));', [username, password, require(__dirname + '/credentials.js').loginKey], function (err, rows, cols) {
+        if (!err) {
+            dbcon.query('insert into user values(?,?);', [username, email], function (err2, rows, cols) {
+                if (err2) {
+                    console.error("User probably exists in user table already");
+                    console.error(err2);
+                    throw err2;
+                }
+            });
 
+        }else {
+            console.error("User probably already exists in login");
+            console.error(err);
+            throw err;
+        }
+    });
+}catch(e){
+    console.error(e);
+    numberOfErrors++;
+}try{
+    dbcon.query('select * from login where username = ? and password = ?', ['TestUsername','testPassword'], function(err, rows, cols){
+        if(err) throw err;
+        if(rows[0]) {
+            console.error("Password wasn't made secure.");
+            numberOfErrors++;
+        }
+    });
+}catch(e){
+    console.error(e);
+    numberOfErrors++;
+}try{
+    let username = "TestUsername";
+    let password = "testPassword";
+    dbcon.query('select * from login where username = ? and password = AES_Encrypt(?,?);',[username,password,require('../credentials.js').loginKey], function(err,rows,cols){
+        console.log(rows);
+        if(!err && rows && rows[0]){
+
+        }else if(!err){
+            console.error('Login system fails at attempt to log in.');
+            numberOfErrors++;
+        }
+        if(err) throw err;
+    });
+}catch(e){
+    console.error(e);
+    numberOfErrors++;
+}try{
+    dbcon.query('delete from login where username = ? and email = ?;', ['TestUsername','testEmail@testdomain.com'], function(err,rows,cols){
+        if(err){
+            console.error('failure to delete testuser -- check if testuser was created in above statement');
+            throw err;
+        }
+    })
+}catch(e){
+    console.error(e);
+    numberOfErrors++;
+}try{
+
+}catch(e){
+    console.error(e);
+    numberOfErrors++;
+}
+assert(numberOfErrors===0,"You have " + numberOfErrors + " happening in this application's test script, please fix them before moving these to release!");
