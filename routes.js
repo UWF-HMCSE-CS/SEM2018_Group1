76c605461058b4d05cda542102ca54d0f1792302
Nova Reaper <chrystechsystems@gmail.com>
2018-10-25 16:29:47 -0500

    Theoretical catch all for any unrouted views

--- routes.js
+++ routes.js
@@ -272,6 +272,11 @@ router.get('/', function (req, res) {
         username: user.username
     });
 });
-
+router.get('/:viewname', function(req,res,next){
+    res.render(req.params.viewname, {}, function(err, html){
+        if(!html) return next();
+        else res.send(html);
+    })
+});
 
 module.exports = router;
